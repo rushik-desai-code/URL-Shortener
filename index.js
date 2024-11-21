79775fe7db68acdfa1c479ce5c0bd9628fe79fe3
Rushik Desai <rushikdesai787@gmail.com>
2024-11-20 16:41:36 -0800

    commit after changing the index file

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const PORT=8000
 
 const app=express()
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cookieParser())
 app.use(checkForAuthentication)
 
 mongooseConnect("mongodb://localhost:27017/UrlMap")
